e80ac2316d8ccaac5c27b5915eee19d5959a8777
HYEOK999 <iasg2004@naver.com>
2020-03-19 02:00:53 +0900

    feat : Added the CSS

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -3,9 +3,9 @@ import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function SearchIcon() {
-  return <FontAwesomeIcon icon={faSearch} />;
+  return <FontAwesomeIcon icon={faSearch} size="lg" />;
 }
 
 export function ShoppingCartIcon() {
-  return <FontAwesomeIcon icon={faShoppingCart} />;
+  return <FontAwesomeIcon icon={faShoppingCart} size="lg" />;
 }
