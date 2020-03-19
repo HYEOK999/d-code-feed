1bd025968acbc0c55d321a28569285b086b79911
HYEOK999 <iasg2004@naver.com>
2020-03-19 23:44:33 +0900

    feat : Added the redux modules

---
+++ src/redux/modules/feed.js
@@ -0,0 +1,71 @@
+import FeedService from '../../services/FeedService';
+import { createAction, createActions, handleActions } from 'redux-actions';
+import { put, call, takeEvery } from 'redux-saga/effects';
+
+const options = {
+  prefix: 'd-code/feed',
+};
+
+// 액션 타입 및 생성자를 한번에 모아서 사용한다. - redux-actions
+// const success = feeds => ({ type: SUCCESS, feeds });
+const { success, pending, fail } = createActions(
+  {
+    SUCCESS: feeds => ({ feeds }),
+  },
+  'PENDING',
+  'FAIL',
+  options
+);
+
+// ACTIONS
+export const getFeeds = createAction('GET_FEEDS');
+
+// 비동기 처리 redux-saga
+function* fetchFeeds() {
+  try {
+    yield put(pending());
+    // await sleep(2000);
+    // yield delay(2000);
+    const res = yield call(FeedService.getList);
+    yield put(success(res.data));
+  } catch (error) {
+    console.log('error : ', error);
+    yield put(fail(error));
+  }
+}
+
+// FEED ROOT SAGA
+export function* feedSaga() {
+  yield takeEvery(getFeeds, fetchFeeds);
+}
+
+// INIITIAL STATE
+const initialState = {
+  feeds: null,
+  loading: false,
+  error: null,
+};
+
+const feed = handleActions(
+  {
+    PENDING: state => ({
+      ...state,
+      loading: true,
+      error: null,
+    }),
+    SUCCESS: (state, action) => ({
+      feeds: action.payload.feeds,
+      loading: false,
+      error: null,
+    }),
+    FAIL: (state, action) => ({
+      ...state,
+      loading: false,
+      error: action.payload,
+    }),
+  },
+  initialState,
+  options
+);
+
+export default feed;
