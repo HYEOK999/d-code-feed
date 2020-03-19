5b373c904962879e72a5734560dee93db337e134
HYEOK999 <iasg2004@naver.com>
2020-03-19 23:47:15 +0900

    feat : Made the Body component

---
+++ src/components/Body/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledMain } from './Styles';
+
+const index = () => {
+  return (
+    <StyledMain>
+      <h2>Feed</h2>
+      <p>최신 패션 뉴스를 전해드립니다.</p>
+    </StyledMain>
+  );
+};
+
+export default index;
