a15de09dcc52fbf58372fdace074dd696ac385f4
HYEOK999 <iasg2004@naver.com>
2020-03-20 14:47:10 +0900

    feat : Added the reducer in combineReducers

--- src/redux/modules/reducer.js
+++ src/redux/modules/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
+import feed from './feed';
 import { connectRouter } from 'connected-react-router';
 
 const reducer = history =>
   combineReducers({
+    feed,
     router: connectRouter(history),
   });
 
