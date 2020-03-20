dbd7edadb1332a04874b4758ea16cc4ce21f02f6
HYEOK999 <iasg2004@naver.com>
2020-03-20 14:48:37 +0900

    feat : Removed the init State

--- src/redux/create.js
+++ src/redux/create.js
@@ -4,26 +4,14 @@ import reducer from './modules/reducer';
 import createSagaMiddleware from 'redux-saga';
 import { createBrowserHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
-import { getFeeds } from './modules/feed';
 import saga from './modules/saga';
 
 export const history = createBrowserHistory();
 const sagaMiddleware = createSagaMiddleware();
 
-const initFeed = () => {
-  getFeeds();
-};
-
 const create = () => {
   const store = createStore(
     reducer(history),
-    {
-      feed: {
-        feeds: initFeed(),
-        loading: false,
-        error: null,
-      },
-    },
     composeWithDevTools(applyMiddleware(routerMiddleware(history), sagaMiddleware))
   );
 
