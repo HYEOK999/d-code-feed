d087fce7161a5851f9f43c05e8ee0704f9898af3
HYEOK999 <iasg2004@naver.com>
2020-03-21 14:03:48 +0900

    feat : Updated the Feed pages

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
+import Head from '../components/Head';
 import Header from '../components/Header';
-import Body from '../components/Body';
-import FeedContainer from '../containers/FeedContainer';
+import FeedListContainer from '../containers/FeedListContainer';
+import Footer from '../components/Footer';
 
 const Feed = () => {
   return (
     <>
+      <Head
+        keywords="d.code, Feed"
+        description="최신 패션 뉴스를 전해드립니다."
+        title="디코드(d.code) 패션 셀렉트샵"
+        favicon="/static/favicon.ico"
+      />
       <Header />
-      <Body />
-      <FeedContainer />
+      <FeedListContainer />
+      <Footer />
     </>
   );
 };
