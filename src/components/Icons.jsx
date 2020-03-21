8c88a8c6973ac2f6c3f08d151302893417a7a4ae
HYEOK999 <iasg2004@naver.com>
2020-03-21 13:59:52 +0900

    feat : Added the Icons

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faCommentAlt, faShareSquare } from '@fortawesome/free-regular-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function SearchIcon() {
@@ -8,4 +10,20 @@ export function SearchIcon() {
 
 export function ShoppingCartIcon() {
   return <FontAwesomeIcon icon={faShoppingCart} size="lg" />;
+}
+
+export function HeartIcon() {
+  return <FontAwesomeIcon icon={faHeart} />;
+}
+
+export function CommentAltIcon() {
+  return <FontAwesomeIcon icon={faCommentAlt} />;
+}
+
+export function LinkIcon() {
+  return <FontAwesomeIcon icon={faShareSquare} />;
+}
+
+export function FacebookIcon() {
+  return <FontAwesomeIcon icon={faFacebook} />;
 }
