15563b5c048311d6e7d0f2bcb6060195af5bc04b
HYEOK999 <iasg2004@naver.com>
2020-03-23 23:58:27 +0900

    feat : Updated the route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
+import Home from './pages/Home';
 import Feed from './pages/Feed';
+import FeedDetail from './pages/FeedDetail';
 import NotFound from './pages/NotFound';
 import ErrorBoundary from 'react-error-boundary';
 import { history } from './redux/create';
@@ -12,8 +14,9 @@ const App = () => (
   <ErrorBoundary FallbackComponent={ErrorFallbackComponent}>
     <ConnectedRouter history={history}>
       <Switch>
+        <Route exact path="/feed/:feedId" component={FeedDetail} />
         <Route exact path="/feed" component={Feed} />
-        <Route exact path="/" component={Feed} />
+        <Route exact path="/" component={Home} />
         <Route component={NotFound} />
       </Switch>
     </ConnectedRouter>
