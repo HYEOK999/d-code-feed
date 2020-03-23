013c0bd325207146cd2d0d610dba10d812bde9c7
HYEOK999 <iasg2004@naver.com>
2020-03-24 01:39:08 +0900

    feat : Removed the annotation in withFeed

--- src/hocs/withFeed.jsx
+++ src/hocs/withFeed.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
-// import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getFeeds } from '../redux/modules/feed';
 
-// HOC에서는
 function withFeed(Component) {
   function WrappedComponent(props) {
     const { feeds } = useSelector(state => state.feed);
@@ -23,6 +21,3 @@ function withFeed(Component) {
 }
 
 export default withFeed;
-
-// 관련없는 props는 패스해주어라.
-// display 이름 설정을 해주어라. (디버깅시 이름을 유지시켜주기위함.)
