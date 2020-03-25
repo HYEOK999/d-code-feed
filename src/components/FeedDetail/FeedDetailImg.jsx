8a09b8f572c1d69257f8445aa9220b5e4950b46c
HYEOK999 <iasg2004@naver.com>
2020-03-25 19:30:30 +0900

    feat : Changed uuid to feed.id

--- src/components/FeedDetail/FeedDetailImg.jsx
+++ src/components/FeedDetail/FeedDetailImg.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import { StyledImgArea, StyledImage } from './Styles';
-import { v4 as uuidv4 } from 'uuid';
 
 const FeedDetailImg = ({ feed }) => {
   return (
     <StyledImgArea>
-      {feed && feed.mediaList.map(img => <StyledImage key={uuidv4()} src={img.url} alt={img.id} />)}
+      {feed && feed.mediaList.map(img => <StyledImage key={img.id} src={img.url} alt={img.id} />)}
     </StyledImgArea>
   );
 };
