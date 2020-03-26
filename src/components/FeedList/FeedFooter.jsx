d99ef60dc13b35e415d7699e8a0cd09e4296804a
HYEOK999 <iasg2004@naver.com>
2020-03-26 23:07:15 +0900

    feat : Optimized

--- src/components/FeedList/FeedFooter.jsx
+++ src/components/FeedList/FeedFooter.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 import { HeartIcon, CommentAltIcon, LinkIcon, FacebookIcon } from '../Icons';
 import { StyledFeedFooter, StyledFeedMdInfo, StyledCountSpan, StyledShareButton } from './Styles';
 
-const FeedFooter = ({ feed }) => {
+const FeedFooter = React.memo(({ mdThumb, mdName, createdAt, id, count }) => {
   return (
     <StyledFeedFooter>
       <StyledFeedMdInfo>
-        <img src={feed.mdInfo.mdThumb} alt="MD 사진" />
-        <figcaption>MD : {feed.mdInfo.mdName}</figcaption>
+        <img src={mdThumb} alt="MD 사진" />
+        <figcaption>MD : {mdName}</figcaption>
       </StyledFeedMdInfo>
-      <span>{feed.createdAt.split(' ')[0]}</span>
+      <span>{createdAt}</span>
       <div>
         <HeartIcon />
-        <StyledCountSpan>{feed.likedCount ? feed.likedCount : '0'}</StyledCountSpan>
+        <StyledCountSpan>{count[0] ? count[0] : '0'}</StyledCountSpan>
         <CommentAltIcon />
-        <StyledCountSpan>{feed.replyCount ? feed.replyCount : '0'}</StyledCountSpan>
+        <StyledCountSpan>{count[1] ? count[1] : '0'}</StyledCountSpan>
         <LinkIcon />
-        <StyledCountSpan>{feed.sharedCount ? feed.sharedCount : '0'}</StyledCountSpan>
+        <StyledCountSpan>{count[2] ? count[2] : '0'}</StyledCountSpan>
       </div>
-      <StyledShareButton id={feed.id}>
+      <StyledShareButton id={id}>
         <FacebookIcon />
         <span>공유하기</span>
       </StyledShareButton>
     </StyledFeedFooter>
   );
-};
+});
 
 export default FeedFooter;
