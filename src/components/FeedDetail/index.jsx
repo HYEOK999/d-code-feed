04325bae3beb92b553e9ae9ea2c8e9af52c99345
HYEOK999 <iasg2004@naver.com>
2020-03-26 23:52:34 +0900

    feat : Optimized.

--- src/components/FeedDetail/index.jsx
+++ src/components/FeedDetail/index.jsx
@@ -6,36 +6,38 @@ import FeedDetailContent from './FeedDetailContent';
 import FeedComment from './FeedComment';
 import Loading from '../Loading';
 
-const FeedDetail = ({ feedId, feeds, comments, loading, error, getFeedComments, setFeedLike }) => {
-  const feed = feeds && feeds.list.filter(feed => feed.id === +feedId)[0];
-  useEffect(() => {
-    getFeedComments();
-  }, [getFeedComments]);
+const FeedDetail = React.memo(
+  ({ feedId, feeds, comments, loading, error, getFeedComments, setFeedLike }) => {
+    const feed = feeds && feeds.list.filter(feed => feed.id === +feedId)[0];
+    useEffect(() => {
+      getFeedComments();
+    }, [getFeedComments]);
 
-  return (
-    <StyledMain>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <h2 className="a11y-hidden">Feed Detail</h2>
-          <FeedDetailImg feed={feed} />
-          <StyledDescriptionArea>
-            <StyledDescriptionArticle>
-              <FeedDetailHeader
-                feed={feed}
-                setFeedLike={setFeedLike}
-                feeds={feeds}
-                feedId={feedId}
-              />
-              <FeedDetailContent feed={feed} />
-              <FeedComment comments={comments} />
-            </StyledDescriptionArticle>
-          </StyledDescriptionArea>
-        </>
-      )}
-    </StyledMain>
-  );
-};
+    return (
+      <StyledMain>
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+            <h2 className="a11y-hidden">Feed Detail</h2>
+            <FeedDetailImg feed={feed} />
+            <StyledDescriptionArea>
+              <StyledDescriptionArticle>
+                <FeedDetailHeader
+                  feed={feed}
+                  setFeedLike={setFeedLike}
+                  feeds={feeds}
+                  feedId={feedId}
+                />
+                <FeedDetailContent feed={feed} />
+                <FeedComment comments={comments} />
+              </StyledDescriptionArticle>
+            </StyledDescriptionArea>
+          </>
+        )}
+      </StyledMain>
+    );
+  }
+);
 
 export default FeedDetail;
